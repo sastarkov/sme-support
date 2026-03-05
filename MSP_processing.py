9f6fe383be343fe002111d0fb3746d6a751551bf
Sergey Starkov <starkov.serge@gmail.com>
2026-03-06 01:49:47 +0300

    начало работы с реестром МСП

---
+++ MSP_processing.py
@@ -0,0 +1,9 @@
+import lxml.etree as ET
+
+
+file_path = "MSP/VO_RRMSPSV_0000_9965_20170210_8ba9b6f2-f58b-49e4-827f-152f0b65d32e.xml"
+
+tree = ET.parse(file_path)
+root = tree.getroot()
+
+print(root.tag)
