0dc65d03c84e28a820d31e5a942f6e1e231170f0
Sergey Starkov <starkov.serge@gmail.com>
2026-03-11 22:50:47 +0300

    Добавлены тесты

--- MSP_aggregator.py
+++ MSP_aggregator.py
@@ -17,14 +17,28 @@ def aggregate_msp(lf):
         # Предыдущая дата включения в реестр
         pl.col("inclusion_date").min().alias("previous_start_date"),
 
-        # Регион – первая мода,чтобы исключить ошибочные записи.
+        # Регион – первая мода,чтобы исключить ошибочные записи
         pl.col("region").mode().first().alias("region"),
       
-        # преимущественная категория 
+        # преимущественная категория в течение года: 1 – микропредприятие, 2 – малое, 3 – среднее
         pl.col("category").mode().first().alias("prefer_category"),
 
+        # тренд изменения категории в течение года (разница категорий в старший месяц и в младший в группе):
+        # 0 - изменений нет, >0 - переход в след. категорию, <0 - переход в предыдущую
+       (pl.col("category")
+           .get(pl.col("month").arg_max())
+           .cast(pl.Int32) -
+         pl.col("category")
+           .get(pl.col("month").arg_min())
+           .cast(pl.Int32)
+        ).alias("category_trend"),
+
+        # преобладающее значение фактора признак нового предприятия в течение года:
+        # 1 – новое предприятие, 2 – нет
         pl.col("sign_new").mode().first().alias("sign_new"),
 
+        # преобладающее значение фактора признак социального предприятия в течение года:
+        # 1 – социальное предприятие, 2 – нет
         pl.col("sign_social").mode().first().alias("sign_social"),
 
         # Средняя численность за первое полугодие (мес. 1-6)
@@ -33,18 +47,19 @@ def aggregate_msp(lf):
         # Средняя численность за второе полугодие (мес. 7-12)
         pl.col("headcount").filter(pl.col("month").is_between(7, 12)).mean().cast(pl.Float64).alias("headcount_2h"),
 
-        # Последнее известное значение основного ОКВЭД (благодаря сортировке берём last())
+        # ОКВЭД – первая мода,чтобы исключить ошибочные записи
         pl.col("main_OKVED").mode().first().alias("main_okved"),
 
+        # владела ли фирма хоть одной лицензией в течение года
         (pl.col("license") == "1").any().alias("has_license")
     ])
 
 
 if __name__ == "__main__":
 
-# 1. Открываем датасет (лениво)
-    lf = pl.scan_parquet("MSP_parsed/",
+    start = time.time()
 
+    lf = pl.scan_parquet("MSP_parsed/",
     schema={
         "inn": pl.String,
         "year": pl.Int32,
@@ -54,24 +69,18 @@ if __name__ == "__main__":
         "category": pl.String,
         "sign_new": pl.String,
         "sign_social": pl.String,
-        "headcount": pl.Int64,
+        "headcount": pl.Float64,
         "main_OKVED": pl.String,
         "other_OKVED": pl.List,
         "license": pl.String
         }
-)  # предполагается, что данные сохранены в этой папке
+).filter(pl.col("year") == 2020)
 
-# 2. Применяем фильтры (год и категория)
-    lf = lf.filter(
-        pl.col("year") == 2020
-           # здесь "3" как строка, в ваших данных может быть число
-    )
-
-    start = time.time()
-
-# 3. Агрегируем
-    result = aggregate_msp(lf).collect()
+    df = aggregate_msp(lf).collect()
 
     end = time.time()
-
     print(f"Время выполнения:{((end-start)/60):.1f} минут.")
+
+    random_inn = df.sample(n=1)['inn'].item()
+
+    df_test = pl.scan_parquet("MSP_parsed/").filter(pl.col("year") == 2020, pl.col("inn") == random_inn).collect()
